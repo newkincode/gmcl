6d7d38da7ccf6e05b9a86d947ebb69a3484eca28
newkincode <maainnewkin59@gmail.com>
2023-03-24 21:36:22 +0900

    함수감지

--- gmcl/lexer/lexer.py
+++ gmcl/lexer/lexer.py
@@ -5,3 +5,10 @@ def lexer(text):
     for i in text:
         code.append(sign.check(i))
     print(code)
+    for i in code:
+        if i  == "lps":
+            try:
+                if "str" in code[code.index(i)-1] and "rps" in code[code.index(i)+1]:
+                    print("함수감지!")
+            except:
+                print("syntaxError : missingParentheses")
--- gmcl/shell.py
+++ gmcl/shell.py
@@ -3,6 +3,8 @@ import lexer.lexer as lexer
 while True:
     try:
         text = input("InterpreterCode:newkinisGMCL> ")
-        if text == "stopCode":break
+        if "exit code=" in text:
+            print("exit code",text.replace("exit code=", ""))
+            break
         lexer.lexer(text)
     except:print("InterpreterError : pythonError")
